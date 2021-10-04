acc56bbe89983c53f442544d788d99774a145627
nakatoconstance <nakatoconstance@gmail.com>
2021-10-04 16:28:25 -0700

    Task 2-source files

--- src/Nakato_CollectFiles.py
+++ src/Nakato_CollectFiles.py
@@ -75,7 +75,7 @@ repo = 'scottyab/rootbeer'
 # Remember to empty the list when going to commit to GitHub.
 # Otherwise they will all be reverted and you will have to re-create them
 # I would advise to create more than one token for repos with heavy commits
-lstTokens = [" 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"]
+lstTokens = [" "]
 #i have commented my token right here----Nakato
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
